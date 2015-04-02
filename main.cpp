14eace99c72f4c572983a2ebe5d72c47f8926565
Kamil Pajak <kontakt@kamilpajak.pl>
2015-04-02 10:16:58 +0200

    Moved blocks of code into separate functions

--- main.cpp
+++ main.cpp
@@ -8,62 +8,15 @@
 #include <vector>
 #include <string>
 
-void printEvent(input_event event) {
-  std::string keyState;
-  if (event.value == 0)
-    keyState = "RELEASED";
-  if (event.value == 1)
-    keyState = "PRESSED";
-  printf("%-8s | %3d | %ld.%06ld\n", keyState.c_str(), event.code,
-         (long)event.time.tv_sec, (long)event.time.tv_usec);
-}
-
 struct keystroke {
   int keyCode;
   double keyDownTime;
   double keyUpTime;
 };
 
-int main(void) {
-  const char *devicePath = "/dev/input/by-id/"
-                           "usb-Microsft_Microsoft_Wireless_Desktop_Receiver_3."
-                           "1-event-kbd";
-  // Otwórz plik
-  int fileDescriptor = open(devicePath, O_RDONLY);
-
-  // Sprawdź czy plik został otwarty poprawnie
-  if (fileDescriptor == -1) {
-    fprintf(stderr, "Cannot open %s: %s\n", devicePath, strerror(errno));
-    return EXIT_FAILURE;
-  }
-
-  std::vector<input_event> events;
+// Wyciągnij keystrokes z listy zdarzeń klawiatury
+std::vector<keystroke> takeKeystrokes(std::vector<input_event> events) {
   std::vector<keystroke> keystrokes;
-
-  int counter = 0;
-
-  while (counter < 90) {
-    struct input_event event;
-    ssize_t numberOfBytesRead = read(fileDescriptor, &event, sizeof event);
-
-    if (numberOfBytesRead == (ssize_t)-1) {
-      if (errno == EINTR)
-        continue;
-      else
-        break;
-    }
-
-    if (numberOfBytesRead != sizeof event) {
-      errno = EIO;
-      break;
-    }
-
-    if (event.type == EV_KEY && (event.value == 0 || event.value == 1)) {
-      events.push_back(event);
-      printEvent(event);
-      counter++;
-    }
-  }
 
   for (int i = 0; i < events.size() - 1; i++)
     if (events[i].value == 1)
@@ -87,6 +40,61 @@ int main(void) {
           keystrokes.push_back(caughtKeystroke);
           break;
         }
+
+  return keystrokes;
+}
+
+void printEvent(input_event event) {
+  std::string keyState;
+  if (event.value == 0)
+    keyState = "RELEASED";
+  if (event.value == 1)
+    keyState = "PRESSED";
+  printf("%-8s | %3d | %ld.%06ld\n", keyState.c_str(), event.code,
+         (long)event.time.tv_sec, (long)event.time.tv_usec);
+}
+
+int main(void) {
+  const char *devicePath = "/dev/input/by-id/"
+                           "usb-Microsft_Microsoft_Wireless_Desktop_Receiver_3."
+                           "1-event-kbd";
+  // Otwórz plik
+  int fileDescriptor = open(devicePath, O_RDONLY);
+
+  // Sprawdź czy plik został otwarty poprawnie
+  if (fileDescriptor == -1) {
+    fprintf(stderr, "Cannot open %s: %s\n", devicePath, strerror(errno));
+    return EXIT_FAILURE;
+  }
+
+  std::vector<input_event> events;
+  std::vector<keystroke> keystrokes;
+
+  int counter = 0;
+
+  while (counter < 11) {
+    struct input_event event;
+    ssize_t numberOfBytesRead = read(fileDescriptor, &event, sizeof event);
+
+    if (numberOfBytesRead == (ssize_t)-1) {
+      if (errno == EINTR)
+        continue;
+      else
+        break;
+    }
+
+    if (numberOfBytesRead != sizeof event) {
+      errno = EIO;
+      break;
+    }
+
+    if (event.type == EV_KEY && (event.value == 0 || event.value == 1)) {
+      events.push_back(event);
+      counter++;
+    }
+  }
+
+  keystrokes = takeKeystrokes(events);
 
   for (int i = 0; i < keystrokes.size(); i++) {
     printf("Key Code:  %i\n", keystrokes[i].keyCode);
