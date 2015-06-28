945b383015b51bcd43e71cd02cfece828fbd4fa1
Kamil Pajak <kontakt@kamilpajak.pl>
2015-06-28 19:05:57 +0200

    implemented nested SELECTs

--- validator.cpp
+++ validator.cpp
@@ -6,24 +6,48 @@
 #include <mysql_driver.h>
 #include <cppconn/prepared_statement.h>
 
+struct sampleFeatures {
+  std::vector<int> downDownTime;
+  std::vector<int> upDownTime;
+  std::vector<int> downUpTime;
+};
+
 int main() {
-  int minimalNumberOfSamples = 10;
+  int usersMinimalNumberOfSamples = 10;
 
-  sql::mysql::MySQL_Driver *driver;
-  driver = sql::mysql::get_mysql_driver_instance();
-
+  sql::mysql::MySQL_Driver *driver = sql::mysql::get_mysql_driver_instance();
+  sql::PreparedStatement *preparedStatement;
   sql::Connection *connection;
+
   connection = driver->connect(HOST, USER, PASSWORD);
   connection->setSchema("typing_dynamics_validator");
 
-  sql::PreparedStatement *preparedStatement;
-  preparedStatement = connection->prepareStatement("SELECT user_id FROM sample GROUP BY user_id HAVING count(*) > ?;");
-  preparedStatement->setInt(1, minimalNumberOfSamples);
-  sql::ResultSet *userIDs;
-  userIDs = preparedStatement->executeQuery();
+  preparedStatement = connection->prepareStatement(
+      "SELECT user_id FROM sample GROUP BY user_id HAVING count(*) > ?;");
+  preparedStatement->setInt(1, usersMinimalNumberOfSamples);
+  sql::ResultSet *userIDs = preparedStatement->executeQuery();
 
   while (userIDs->next()) {
     std::cout << userIDs->getInt("user_id") << std::endl;
+    preparedStatement =
+        connection->prepareStatement("SELECT id FROM sample WHERE user_id = ?");
+    preparedStatement->setInt(1, userIDs->getInt("user_id"));
+    sql::ResultSet *sampleIDs = preparedStatement->executeQuery();
+
+    while (sampleIDs->next()) {
+      std::cout << sampleIDs->getInt("id") << std::endl;
+      preparedStatement = connection->prepareStatement(
+          "SELECT keyDownTime, keyUpTime FROM keystroke WHERE sample_id = ?");
+      preparedStatement->setInt(1, sampleIDs->getInt("id"));
+      sql::ResultSet *keystrokeData = preparedStatement->executeQuery();
+
+      while (keystrokeData->next()) {
+        std::cout << keystrokeData->getDouble("keyDownTime") << " - "
+                  << keystrokeData->getDouble("keyUpTime") << std::endl;
+      }
+      delete keystrokeData;
+    }
+    delete sampleIDs;
   }
 
   delete userIDs;
