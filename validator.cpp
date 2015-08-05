bac55cce6fd14e0d7a2c075f0e17a991a04cb846
Kamil Pajak <kontakt@kamilpajak.pl>
2015-08-05 23:43:11 +0200

    moved code that takes data from sql into separate function. Changed data stuctures

--- validator.cpp
+++ validator.cpp
@@ -14,8 +14,6 @@ struct Keystroke {
 };
 
 struct Sample {
-  int userID_SQL;
-  int sampleID_SQL;
   std::vector<double> downDownTimes;
   std::vector<double> upDownTimes;
   std::vector<double> downUpTimes;
@@ -74,6 +72,59 @@ std::vector<double> takeDownUpTimes(std::vector<Keystroke> keystrokes) {
   }
 
   return downUpTimes;
+}
+
+std::map<int, std::map<int, Sample>> summarizeKeystrokes(int minimumSamples) {
+  std::map<int, std::map<int, Sample>> samples;
+
+  sql::mysql::MySQL_Driver *driver = sql::mysql::get_mysql_driver_instance();
+  sql::PreparedStatement *preparedStatement;
+  sql::Connection *connection;
+
+  connection = driver->connect(HOST, USER, PASSWORD);
+  connection->setSchema("typing_dynamics_validator");
+
+  preparedStatement = connection->prepareStatement("SELECT user_id FROM sample GROUP BY user_id HAVING count(*) > ?;");
+  preparedStatement->setInt(1, minimumSamples);
+  sql::ResultSet *userIDs = preparedStatement->executeQuery();
+
+  while (userIDs->next()) {
+    preparedStatement = connection->prepareStatement("SELECT id FROM sample WHERE user_id = ?");
+    preparedStatement->setInt(1, userIDs->getInt("user_id"));
+    sql::ResultSet *sampleIDs = preparedStatement->executeQuery();
+
+    while (sampleIDs->next()) {
+      preparedStatement = connection->prepareStatement("SELECT keyCode, keyDownTime, keyUpTime FROM keystroke WHERE sample_id = ?");
+      preparedStatement->setInt(1, sampleIDs->getInt("id"));
+      sql::ResultSet *keystrokeIDs = preparedStatement->executeQuery();
+
+      std::vector<Keystroke> keystrokes;
+      while (keystrokeIDs->next()) {
+        Keystroke keystroke;
+        keystroke.keyCode = keystrokeIDs->getInt("keyCode");
+        keystroke.keyDownTime = keystrokeIDs->getDouble("keyDownTime");
+        keystroke.keyUpTime = keystrokeIDs->getDouble("keyUpTime");
+        keystrokes.push_back(keystroke);
+      }
+
+      delete keystrokeIDs;
+      int userID = userIDs->getInt("user_id");
+      int sampleID = sampleIDs->getInt("id");
+      Sample sample;
+      sample.downDownTimes = takeDownDownTimes(keystrokes);
+      sample.upDownTimes = takeUpDownTimes(keystrokes);
+      sample.downUpTimes = takeDownUpTimes(keystrokes);
+      samples[userID][sampleID] = sample;
+    }
+
+    delete sampleIDs;
+  }
+
+  delete userIDs;
+  delete preparedStatement;
+  delete connection;
+
+  return samples;
 }
 
 Profile takeProfile(std::vector<Sample> samples, int beginOfRange,
@@ -135,7 +186,7 @@ Profile takeProfile(std::vector<Sample> samples, int beginOfRange,
   }
 
   Profile profile;
-  profile.userID_SQL = inputSamples[0].userID_SQL;
+
   profile.downDownMeans = downDownMeans;
   profile.upDownMeans = upDownMeans;
   profile.downUpMeans = downUpMeans;
@@ -198,76 +249,8 @@ Threshold determineThreshold(Profile profile) {
 }
 
 int main() {
-  int minimalNumberOfSamples = 10;
-  int samplesPerProfile = 10;
-  std::vector<Sample> samples;
-  std::vector<Profile> profiles;
-
-  // Take keystroke data from MySQL server
-  sql::mysql::MySQL_Driver *driver = sql::mysql::get_mysql_driver_instance();
-  sql::PreparedStatement *preparedStatement;
-  sql::Connection *connection;
-
-  connection = driver->connect(HOST, USER, PASSWORD);
-  connection->setSchema("typing_dynamics_validator");
-
-  preparedStatement = connection->prepareStatement(
-      "SELECT user_id FROM sample GROUP BY user_id HAVING count(*) > ?;");
-  preparedStatement->setInt(1, minimalNumberOfSamples);
-  sql::ResultSet *userIDs = preparedStatement->executeQuery();
-
-  while (userIDs->next()) {
-    preparedStatement =
-        connection->prepareStatement("SELECT id FROM sample WHERE user_id = ?");
-    preparedStatement->setInt(1, userIDs->getInt("user_id"));
-    sql::ResultSet *sampleIDs = preparedStatement->executeQuery();
-
-    while (sampleIDs->next()) {
-      preparedStatement =
-          connection->prepareStatement("SELECT keyCode, keyDownTime, keyUpTime "
-                                       "FROM keystroke WHERE sample_id = ?");
-      preparedStatement->setInt(1, sampleIDs->getInt("id"));
-      sql::ResultSet *keystrokeSQL = preparedStatement->executeQuery();
-
-      std::vector<Keystroke> keystrokes;
-      while (keystrokeSQL->next()) {
-        Keystroke keystroke;
-        keystroke.keyCode = keystrokeSQL->getInt("keyCode");
-        keystroke.keyDownTime = keystrokeSQL->getDouble("keyDownTime");
-        keystroke.keyUpTime = keystrokeSQL->getDouble("keyUpTime");
-        keystrokes.push_back(keystroke);
-      }
-
-      delete keystrokeSQL;
-      Sample sample;
-      sample.userID_SQL = userIDs->getInt("user_id");
-      sample.sampleID_SQL = sampleIDs->getInt("id");
-      sample.downDownTimes = takeDownDownTimes(keystrokes);
-      sample.upDownTimes = takeUpDownTimes(keystrokes);
-      sample.downUpTimes = takeDownUpTimes(keystrokes);
-      samples.push_back(sample);
-    }
-
-    delete sampleIDs;
-  }
-
-  delete userIDs;
-  delete preparedStatement;
-  delete connection;
-
-  // Take all profiles
-  for (unsigned int i = 0; i <= samples.size() - samplesPerProfile; i++) {
-    if (samples[i].userID_SQL == samples[i + samplesPerProfile - 1].userID_SQL)
-      profiles.push_back(takeProfile(samples, i, i + samplesPerProfile - 1));
-  }
-
-  int profileIndex = 221;
-  int sampleIndex = 0;
-
-  std::cout << "Profile of user " << profiles[profileIndex].userID_SQL << " vs sample of user " << samples[sampleIndex].userID_SQL << std::endl;
-  std::cout << "Distances (DD, UD, DU):" << std::endl;
-  Distance distance = calculateDistance(profiles[profileIndex], samples[sampleIndex]);
-  std::cout << distance.downDown << ", " << distance.upDown << ", " << distance.downUp << std::endl;
+  std::map<int, std::map<int, Sample>> samples;
+  samples = summarizeKeystrokes(10);
 
   return 0;
 }
